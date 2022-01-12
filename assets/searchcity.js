5ebea153f347e43486df690d950c6ea062529086
rwood022 <93175265+rwood022@users.noreply.github.com>
2022-01-11 23:53:16 -0500

    added logic to seach btn

--- assets/searchcity.js
+++ assets/searchcity.js
@@ -1,6 +1,23 @@
-var APIkey = f908e2482b3256d6ec8a552a28745317;
-var city
+// var APIkey = f908e2482b3256d6ec8a552a28745317;
+// var city
+// function searchApi()
+// api.openweathermap.org/data/2.5/weather?q={city name}&appid={API key}
 
+var searchCity = document.querySelector("#search-city");
 
+function handleSearchFormSubmit(event) {
+    event.preventDefault();
 
-function searchApi()
+    var searchInputVal = document.querySelector("#search-input").value;
+
+    if (!searchInputVal) {
+        console.error("You need to type the name of a US city!");
+        return;
+    }
+
+    var queryString ="./"
+}
+
+searchCity.addEventListener("submit", handleSearchFormSubmit);
+
+    
