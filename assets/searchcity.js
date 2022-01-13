892728596f4951f89fb3e5e0b8b3d926fa24136e
rwood022 <93175265+rwood022@users.noreply.github.com>
2022-01-12 22:24:54 -0500

    added icon http and decription

--- assets/searchcity.js
+++ assets/searchcity.js
@@ -1,16 +1,36 @@
 var rootUrl = "https://api.openweathermap.org";
-var APIkey = f908e2482b3256d6ec8a552a28745317;
+var APIkey = "f908e2482b3256d6ec8a552a28745317";
 
-
+// on local HTML
 var searchCity = document.querySelector("#search-city");
+
+
+function renderCurrentData (city, weather, timezone) {
+  var date = dayjs().tz(timezone).format("M/D/YYYY");
+
+    var tempFar = weather.temp;
+    var windMph = weather.wind_speed;
+    var humidity = weather.humdity;
+    var uvi = weather.uvi; 
+    var icon = `http://openweathermap.org/img/w/${weather.weather[0].icon}.png`;
+    var iconDescript =  weather.weather[0].description || weather[0].main;
+}
+
+function fetch(searchCity) {
+    var apiUrl = `${rootUrl}`
+}
+
+
+
 
 function handleSearchFormSubmit(event) {
     event.preventDefault();
+    var search = searchInputVal.value.trim();
 
     var searchInputVal = document.querySelector("#search-input").value;
 
     if (!searchInputVal) {
-        console.error("You need to type the name of a US city!");
+        console.error("You need to type the name of a city!");
         return;
     }
 
@@ -19,4 +39,4 @@ function handleSearchFormSubmit(event) {
 
 searchCity.addEventListener("submit", handleSearchFormSubmit);
 
-    
+
