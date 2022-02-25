3c63357dfc2b50a5a552ee8463e464995a7298fd
rwood022 <93175265+rwood022@users.noreply.github.com>
2022-02-25 18:49:34 -0500

    added readme

--- README.md
+++ README.md
@@ -1,1 +1,23 @@
 # Weather-now
+
+
+
+## Description
+There's no point during the day when we are not concerned about weather conditions. Whether we are travelling, deciding on a fun outdoor activity, or simply want to know how to dress for the day. Weather effects our daily lives and creating an application that is both useful and current is an extremely important tool to have as a Web Developer.  
+The reason I wanted to build this application was to further my understanding of API addresses and create an application that will always stay current. 
+Using an applicaton like 'Weather Now' will help anyone who needs to quickly know the weather conditions of any city in the world.
+I learned how to call API addresses using Javascript. I also spent many hours debugging and troubleshooting my applicatoin.  
+
+## Table of Contents
+
+## Installation
+The user can open my application by deploying it from the gitHub page. Once open the user can type in their desired city in the seacrh bar and press search. This will render the city's curent temp, forecast, humidity, wind speed, and uv index. 
+
+## Usage
+A user can use my application anytime they need to know the weather in their current location. 
+
+## Credits
+I was able to complete this project with the help from my tutor, and a references in-class activities at Georgia Tech Full-Stack Web Development Bootcamp.  
+
+## License
+MIT
--- assets/searchcity.js
+++ assets/searchcity.js
@@ -16,22 +16,33 @@ console.log(renderSearch);
         });
     }
 
+    function fetchUVI (uvi) {
+        fetch (
+            `https://api.openweathermap.org/data/2.5/onecall?lat={lat}&lon={lon}&exclude={part}&appid=` + APIkey
+        )
+        .then((response) => response.json())
+        .then((data) => {
+            console.log(data);
+            display(data);
+        })
+    }
+
 
    function display (data){
         var { name } = data;
-        // var { forecast } = data.weather[0];
+        // var { current } = current.uvi;
         var { temp, humidity } = data.main;
         var { speed }= data.wind;
-        // console.log(town,forecast,temp,humidity,windSpeed);
         document.querySelector(".town").innerHTML = "Weather in " + name;
-        // document.querySelector(".forecast").innerHTML = "Currently: " + forecast;
         document.querySelector(".temp").innerHTML = temp + "F";
         document.querySelector(".humidity").innerHTML = "Humidity: " + humidity + "%";
         document.querySelector(".windSpeed").innerHTML = "Wind Speed:" + speed + "mph";
+        // document.querySelector(".uvIndex").innerHTML = "Current UV Index:" + current;
     }
 function searchCity() {
     console.log("click");
         fetchWeather(document.querySelector("#search-input").value);
+        // fetchUVI(document.querySelector("#search-input").value);
     }
 
 
@@ -41,191 +52,4 @@ renderSearch.addEventListener("click", function(e){
 });
 
 
-
-// var previousViews = [];
-
-// // on local HTML
-// var searchCity = document.querySelector("search-city");
-// var searchInputVal = document.querySelector("search-input");
-// var currentDayCard = document.querySelector("current-day");
-// // var fiveDayCards = document.querySelector("#iveDay");
-// var previousViewsContainer = document.querySelector("search-list");
-
-// dayjs.extend(window.dayjs_plugin_utc);
-// dayjs.extend(window.dayjs_plugin_timezone);
-
-
-// var app = {
-//     init: function() {
-//         document.getElementById("#btnSearch");
-//         document.addEventListener("click", fetchWeather);
-//         document.getElementById("#btnSearch");
-//         document.addEventListener("click", fetchCoordinates);
-//     };
-
-//     function fetchWeather(location) {
-//         var { lat } = location;
-//         var { lon } = location;
-//         var city = location.name;
-//         var units = metric;
-//         var lang = en;
-//         var apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&exclude={part}&units=metric&appid=${APIkey}`;
-//         console.log(location);
-
-    
-
-
-//         fetch(apiUrl)
-//             .then(function (response) {
-//             return response.json();
-//              })
-
-        
-//             .then(function (data) {
-//             if (!data[0]) {
-//                 alert("Location not found")
-//             } else {
-//                 appendToHistory(search);
-//                 displayWeather(data[0]);
-//             }
-//             .catch(function (err) {
-//                 console.error(err);
-//             });
-       
-            
-    
-// //         displayWeather(function (response) {
-// //             console.log(response);
-// //             var today = document.querySelector("#current-day");
-
-// //         });        
-        
-    
-        
-        
-    
-
-// // search history
-// //     function previousViews() {
-// //         previousViewsContainer.innerHTML = "";
-
-// //         for (var i = previousViews.length - 1; i >= 0; i--) {
-// //             var button = document.createElement("button");
-// //             button.setAttribute("type", "button");
-// //             button.setAttribute( "aria-controls", "current day forecast");
-// //             button.classList.add("search-list", "search-list");
-// //             button.setAttribute("get-data", previousViews[i]);
-// //             button.textContent = previousViews[i];
-// //             previousViewsContainer.append(button);
-// //             console.log("get-data");
-// //         }
-// //     }
-
-// //     function renderCurrentData (city, weather, timezone) {  console.log(city, weather, timezone);
-
-// //     var date = dayjs().tz(timezone).format("M/D/YYYY");
-
-// //         var tempFar = weather.temp;
-// //         var windMph = weather.wind_speed;
-// //         var humidityWea = weather.humdity;
-// //         var uvi = weather.uvi; 
-// //         var icon = `http://openweathermap.org/img/w/${weather.weather[0].icon}.png`;
-// //         var iconDescript =  weather.weather[0].description || weather[0].main;
-
-// //         var box = document.createElement("div");
-// //         var boxBody = document.createElement("div");
-// //         var date = document.createElement("h1");
-// //         var iconPic = document.createElement("img");
-// //         var temp = document.createElement("li");
-// //         var wind = document.createElement("li");
-// //         var humid = document.createElement("li");
-// //         var uvIndex = document.createElement("li");
-
-//         box.setAttribute("class", "box");
-//         boxBody.setAttribute("class", "box-body");
-//         box.append(boxBody);
-
-//         date.setAttribute("class", "h1");
-//         temp.setAttribute("class", "box-text");
-//         wind.setAttribute("class", "box-text");
-//         humidity.setAttribute("class", "box-text");
-
-//         date.textContent = `${city} (${date})`;
-//         iconPic.setAttribute("src", icon);
-//         icon.setAttribute("alt", iconDescript);
-//         iconPic.setAttribute("class", "iconPic");
-//         date.append(iconPic);
-//         temp.textContent =`Temp: ${tempFar}F`;
-//         wind.textContent = `Wind: ${windSpeed}MPH`;
-//         humidityWea.textContent = `Humiditiy: ${humidity}%`;
-//         boxBody.append(date, temp, wind, humidityWea);
-//         uvIndex.textContent = "UV Index: ";
-//         uviBadge.classList.add("btn");
-
-//         if (uvi < 3) {
-//             uviBadge.classList.add("btn-success");
-//         } else if (uvi < 7) {
-//             uviBadge.classList.add("btn-warning");
-//         } else {
-//             uviBade.classList.add("btn-danger");
-//         }
-//         uviBadge.textContent = uvi;
-//         uviIndex.append(uviBadge);
-//         boxBody.append(uvIndex);
-
-//     }
-// //lat and long values to fetch the weather
-
-//     function fetchCoordinates(search) {
-//         var apiUrl = `https://api.openweathermap.org/geo/1.0/direct?q=${search}&limit=5&appid=${APIkey}`;
-    
-
-//         fetch(apiUrl)
-//         .then(function (res){
-//         return res.json();
-//         })
-//         .then(function (data){
-//         if (!data[0]) {
-//             alert("invalid");
-//         } else {
-//             fetchWeather(data[0]);
-//         }
-//         })
-//         .catch(function (err) {
-//         console.error(err);
-//          });
-//     }
-
-
-//     function handleSearchFormSubmit(event) {
-//         event.preventDefault();
-
-//         var searchInputVal = document.querySelector("#search-input").value;
-
-    
-//         searchInputVal.value = "";
-//         console(searchInputVal.value);
-
-
-//         if (!searchInputVal.value) {
-//             console.error("You need to type the name of a city!");
-//             return;
-//         }
-//         fetchCoordinates(search);
-//     }
-    
-// };
-
-// function handlepreviousSearch(event) {
-//     if(!event.target.matches(".search-list")) {
-//         return;
-//     }
-
-//     var button = event.target;
-//     var searchCity = button.getAttribut("get-data");
-//     fetchCoordinates(searchCity);
-// }
-
-// previousViews();
-// searchCity.addEventListener("submit", handleSearchFormSubmit);
 
