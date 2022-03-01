09f6462cba5c922cb37d5e628ed7865b27e4ed62
rwood022 <93175265+rwood022@users.noreply.github.com>
2022-03-01 17:31:04 -0500

    added icon and uvi

--- assets/searchcity.js
+++ assets/searchcity.js
@@ -13,44 +13,54 @@ console.log(renderSearch);
         .then((data) => {
             console.log(data);
             display(data);
+            fetchUVI(data.coord.lat, data.coord.lon)
         });
     }
 
-    // function fetchLatLon (lat, lon) {
-    //     fetch (
-    //         `http://api.openweathermap.org/geo/1.0/reverse?lat=${lat}&lon=${lon}&limit={limit}&appid=` + APIkey
-    //     )
-    //     .then((response) => response.json())
-    //     .then((data) => {
-    //         console.log(lat, lon);
-    //         display(data);
-    //     });
-    // }
+    function fetchFive (fiveDayArray) {
+        console.log(fiveDayArray);
+        for (var i = 0; i < 5; i++) {
+            console.log(fiveDayArray[i]);
+            var cardContainer = document.createElement("div");
+            cardContainer.classList.add("five");
+            cardContainer.innerHTML = 
+            ` <div id="day1">Day 1 </div>
+            <div class="temp">${fiveDayArray[i].temp.day} </div>
+            <img src="http://openweathermap.org/img/w/${fiveDayArray[i].weather[0].icon}.png" alt="" class="icon" />
+            <div class="humidity">${fiveDayArray[i].humidity} </div>
+            <div class="windSpeed">${fiveDayArray[i].wind_speed}</div>`
+            document.querySelector(".five-day").appendchild(cardContainer)
+        }
+    }
 
-    function fetchUVI (uvi) {
+    function fetchUVI (lat, lon) {
         fetch (
-            `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&exclude=minutely&appid=` + APIkey
+            `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&exclude=minutely,hourly,alerts&units=imperial&appid=` + APIkey
         )
         .then((response) => response.json())
         .then((data) => {
-            console.log(data);
-            display(data);
+            console.log("fetch uvi", data);
+            // display(data);
+            var currentUVI = data.current.uvi;
+            document.querySelector(".uvIndex").innerHTML = " UV index:" + currentUVI;
+            fetchFive(data.daily);
         })
     }
 
 
    function display (data){
         var { name } = data;
-        // var { current } = current.uvi;
+        
         var { icon, description } = data.weather[0];
+        console.log(data.weather[0]);
         var { temp, humidity } = data.main;
         var { speed }= data.wind;
         var { lat, lon } = data.coord;
         console.log(icon, description, lat, lon);
         document.querySelector(".town").innerHTML = "Weather in " + name;
         document.querySelector(".temp").innerHTML = temp + "F";
-        document.querySelector(".icon").src = `https://openweather.org/img/w/${data.weather[0].icon}png`;
-        document.querySelector(".description").innerHTML = description;
+        document.querySelector(".icon").src = `http://openweathermap.org/img/w/` + data.weather[0].icon + `.png`;
+        document.querySelector(".description").innerHTML = data.weather[0].description;
         document.querySelector(".humidity").innerHTML = "Humidity: " + humidity + "%";
         document.querySelector(".windSpeed").innerHTML = "Wind Speed:" + speed + "mph";
         // document.querySelector(".uvIndex").innerHTML = "Current UV Index:" + current;
